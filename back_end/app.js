f069159514f24c0cc64f0a669f12a8272761c6ed
Mohammed <moalaa2032011@gmail.com>
2026-06-30 02:38:46 +0300

    configue vereel deployement

--- back_end/app.js
+++ back_end/app.js
@@ -2,11 +2,14 @@ import express from "express";
 import todoRoutes from "./routes/todoRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import errorHandler from "./middleware/errorHandler.js";
+import connectDB from "./config/db.js";
 import cors from "cors";
+
 const app = express();
 
-app.use(cors());
+await connectDB();
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -14,8 +17,8 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/todos", todoRoutes);
-
 app.use("/api/users", authRoutes);
 
 app.use(errorHandler);
+
 export default app;
